c9adc2e51cfe45c0f071991c26e223a414f11174
Atharv <15321@stjoebears.com>
2024-01-25 21:18:00 -0500

    Added Homing to Hard Stop to the elevation - Atharv

--- src/main/java/frc/robot/subsystems/ArmElevationSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmElevationSubsystem.java
@@ -4,13 +4,15 @@
 
 package frc.robot.subsystems;
 
-
-
 import org.usfirst.frc3620.misc.CANSparkMaxSendable;
 import org.usfirst.frc3620.misc.RobotMode;
 import org.usfirst.frc3620.misc.Utilities;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+
+import edu.wpi.first.wpilibj.Timer;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
@@ -23,44 +25,103 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmElevationSubsystem extends SubsystemBase {
   /** Creates a new ArmElevationSubsystem. */
-  
-    CANSparkMaxSendable motor;
-    RelativeEncoder motorEncoder;
 
+  CANSparkMaxSendable motor;
+  RelativeEncoder motorEncoder;
+  Timer calibrationTimer;
 
-    int elevateEncoderValueAt90Degrees;
+  SparkPIDController pid = null;
 
-    final double kP = 0; //change PID later
-    final double kI = 0;
-    final double kD = 0;
+  boolean encoderCalibrated = false;
+  int elevateEncoderValueAt90Degrees;
 
-    final PIDController m_pidController = new PIDController(kP, kI, kD);
+  Double requestedPositionWhileCalibrating = null;
 
-    double elevationOffset;
+  final double kP = 0.4; // change PID later
+  final double kI = 0;
+  final double kD = 0;
+  final double kFF = 0;
 
-    double requestedPosition = 0; //degrees change later
+  final double outputLimit = 0.2;
 
-    final String name = "Arm Elevate";
-  
-    public ArmElevationSubsystem(CANSparkMaxSendable motor){
+  double elevationOffset;
 
-      this.motor = motor;
-      this.motorEncoder = motor.getEncoder();
-      
+  double requestedPosition = 0; // degrees change later
 
-       }
-      
+  final String name = "Arm Elevate";
+
+  public ArmElevationSubsystem(CANSparkMaxSendable motor) {
+
+    this.motor = motor;
+    this.motorEncoder = motor.getEncoder();
+
+    pid = motor.getPIDController();
+
+    pid.setP(kP); // 0.1
+    pid.setI(kI); // 0.0
+    pid.setD(kD); // 10
+    pid.setFF(kFF); // 0.0
+
+    pid.setOutputRange(-outputLimit, outputLimit);
+
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    SmartDashboard.putBoolean(name + ".calibrated", encoderCalibrated);
+
+    if (motor != null) {
+      SmartDashboard.putNumber(name + ".current", motor.getOutputCurrent());
+      SmartDashboard.putNumber(name + ".power", motor.getAppliedOutput());
+      SmartDashboard.putNumber(name + ".temperature", motor.getMotorTemperature());
+
+      if (motorEncoder != null) {
+        double spinSpeed = motorEncoder.getVelocity();
+        double spinPosition = motorEncoder.getPosition();
+        SmartDashboard.putNumber(name + ".speed", spinSpeed);
+        SmartDashboard.putNumber(name + ".position", spinPosition);
+
+        if (Robot.getCurrentRobotMode() == RobotMode.TELEOP || Robot.getCurrentRobotMode() == RobotMode.AUTONOMOUS) {
+          if (!encoderCalibrated) {
+            setPower(0.03);
+
+            if (calibrationTimer == null) {
+              calibrationTimer = new Timer();
+              calibrationTimer.reset();
+              calibrationTimer.start();
+            } else {
+              if (calibrationTimer.get() > 0.5) {
+                if (Math.abs(spinSpeed) < 2) {
+                  encoderCalibrated = true;
+                  setPower(0.0);
+                  motorEncoder.setPosition(0.0);
+                  if (requestedPositionWhileCalibrating != null) {
+                    setElevation(requestedPositionWhileCalibrating);
+                    requestedPositionWhileCalibrating = null;
+                  } else {
+                    // this might try to extend arm while vertical, a big no-no!
+                    // setLength(encoder.getPosition());
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 
   public void setElevation(double elevation) {
     elevation = MathUtil.clamp(elevation, -45, 200);
     SmartDashboard.putNumber(name + ".requestedElevation", elevation);
     requestedPosition = elevation;
-
-    m_pidController.setSetpoint(elevation);
+    if (encoderCalibrated) {
+      pid.setReference(elevation, ControlType.kPosition);
+    } else {
+      requestedPositionWhileCalibrating = elevation;
+    }
   }
 
   public double getRequestedElevation() {
@@ -71,19 +132,18 @@ public class ArmElevationSubsystem extends SubsystemBase {
     motor.set(power);
   }
 
-  public double getCurrentPosition(){
-    if (motorEncoder == null) return 0;
-		double motorEncoderValue = motorEncoder.getPosition();
+  public double getCurrentPosition() {
+    if (motorEncoder == null)
+      return 0;
+    double motorEncoderValue = motorEncoder.getPosition();
     // converting heading from tics (ranging from 0 to 4095) to degrees
-		double position = (motorEncoderValue - elevateEncoderValueAt90Degrees)*(360.0/4096.0) + 90;
+    double position = (motorEncoderValue - elevateEncoderValueAt90Degrees) * (360.0 / 4096.0) + 90;
     // get it into the -180..180 range
     position = Utilities.normalizeAngle(position);
     // get it into the -90..270 range
-    if (position< -90){
+    if (position < -90) {
       position = position + 360;
     }
-		return position;
-	}
+    return position;
+  }
 }
-
-
