8036441dca399a7fa4a136b16a0ed688e71b4a45
Albert <12158@stjoebears.com>
2024-02-05 19:29:28 -0500

    Transfer for camera

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import java.io.IOException;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
 
 //import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose3d;
@@ -33,8 +34,10 @@ public class VisionSubsystem extends SubsystemBase {
 
   AprilTagFieldLayout fieldLayout;
 
+  
+  int PipelineIndex = 0;
   Double camHeight = 1.0;//Ft, change accordingly
-  Double angCamToNote = 30.0;//Degrees, change accordingly
+  Double angCam = 30.0;//Degrees, change accordingly
  
   
    public static final double targetWidth =
@@ -73,68 +76,86 @@ public class VisionSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
+        
+  cam.setPipelineIndex(PipelineIndex);
     // This method will be called once per scheduler run
     var res = cam.getLatestResult();
     if (res.hasTargets()) {
-        var imageCaptureTime = res.getTimestampSeconds();
-        var bestTarget = res.getBestTarget();
-        SmartDashboard.putNumber("target.id", bestTarget.getFiducialId());
-        SmartDashboard.putNumber("target.area", bestTarget.getArea());
 
-       // int bestTargetID = bestTarget.getFiducialId(); //gets target ID number
+        if(PipelineIndex == 0){//apriltags
 
-
-       // SmartDashboard.putNumber("bestTargetID", bestTargetID);
-
-        var camToTargetTransform = bestTarget.getBestCameraToTarget();
-        SmartDashboard.putNumber("transform.x", camToTargetTransform.getX());
-        SmartDashboard.putNumber("transform.y", camToTargetTransform.getY());
-        SmartDashboard.putNumber("transform.z", camToTargetTransform.getZ());
-        Rotation3d rotation3d = camToTargetTransform.getRotation();
-        SmartDashboard.putNumber("rotation.x", rotation3d.getX());
-        SmartDashboard.putNumber("rotation.y", rotation3d.getY());
-        SmartDashboard.putNumber("rotation.z", rotation3d.getZ());
-        //var camPose = Constants.kFarTargetPose.transformBy(camToTargetTrans.inverse());
-
-        var bestTagPose = fieldLayout.getTagPose(bestTarget.getFiducialId()); //Position of best tag on field
-
-         if( bestTagPose.isPresent()){
-
-                var bestTagPoseValue = bestTagPose.get();//Getting true value of bestTagPose
-                Transform3d targetToCamTransform = camToTargetTransform.inverse();
-
-                var camPose = bestTagPoseValue.plus(targetToCamTransform);
-
-                SmartDashboard.putNumber("tagPose.X",bestTagPoseValue.getX()); //X of Tag
-                SmartDashboard.putNumber("tagPose.Y",bestTagPoseValue.getY()); //Y of Tag
-                SmartDashboard.putNumber("tagPose.Z",bestTagPoseValue.getZ()); //Z of Tag
-                
-                Rotation3d rotationCam = camPose.getRotation();
-
-                //Future: Incorperate with gyro on robot for angle and more accurate distance
-
-                //printing current camera pose and rotation
-                SmartDashboard.putNumber("robotPose.X",camPose.getX());
-                SmartDashboard.putNumber("robotPose.Y",camPose.getY());
-                SmartDashboard.putNumber("robotPose.Z",camPose.getZ());
-
-                SmartDashboard.putNumber("rotationOfRobot.x", rotationCam.getX()); //if robot flips(andrew)
-                SmartDashboard.putNumber("rotationOfRobot.y", rotationCam.getY()); //up and down
-                SmartDashboard.putNumber("rotationOfRobot.z", rotationCam.getZ()); //left and right
-                
-               
-             
-        }
-      
-    }
-       /* if (res.hasTargets()) {//for object detection TODO: find out how to determine what is an object
+                var imageCaptureTime = res.getTimestampSeconds();
                 var bestTarget = res.getBestTarget();
-
                 SmartDashboard.putNumber("target.id", bestTarget.getFiducialId());
+                SmartDashboard.putNumber("target.area", bestTarget.getArea());
+
+                // int bestTargetID = bestTarget.getFiducialId(); //gets target ID number
+
+
+                // SmartDashboard.putNumber("bestTargetID", bestTargetID);
 
                 var camToTargetTransform = bestTarget.getBestCameraToTarget();
+                SmartDashboard.putNumber("transform.x", camToTargetTransform.getX());
+                SmartDashboard.putNumber("transform.y", camToTargetTransform.getY());
+                SmartDashboard.putNumber("transform.z", camToTargetTransform.getZ());
+                Rotation3d rotation3d = camToTargetTransform.getRotation();
+                SmartDashboard.putNumber("rotation.x", rotation3d.getX());
+                SmartDashboard.putNumber("rotation.y", rotation3d.getY());
+                SmartDashboard.putNumber("rotation.z", rotation3d.getZ());
+                //var camPose = Constants.kFarTargetPose.transformBy(camToTargetTrans.inverse());
 
+                var bestTagPose = fieldLayout.getTagPose(bestTarget.getFiducialId()); //Position of best tag on field
 
-        }*/
-  }
+                if( bestTagPose.isPresent()){
+
+                        var bestTagPoseValue = bestTagPose.get();//Getting true value of bestTagPose
+                        Transform3d targetToCamTransform = camToTargetTransform.inverse();
+
+                        var camPose = bestTagPoseValue.plus(targetToCamTransform);
+
+                        SmartDashboard.putNumber("tagPose.X",bestTagPoseValue.getX()); //X of Tag
+                        SmartDashboard.putNumber("tagPose.Y",bestTagPoseValue.getY()); //Y of Tag
+                        SmartDashboard.putNumber("tagPose.Z",bestTagPoseValue.getZ()); //Z of Tag
+                
+                        Rotation3d rotationCam = camPose.getRotation();
+
+                        //Future: Incorperate with gyro on robot for angle and more accurate distance
+
+                        //printing current camera pose and rotation
+                        SmartDashboard.putNumber("robotPose.X",camPose.getX());
+                        SmartDashboard.putNumber("robotPose.Y",camPose.getY());
+                        SmartDashboard.putNumber("robotPose.Z",camPose.getZ());
+
+                        SmartDashboard.putNumber("rotationOfRobot.x", rotationCam.getX()); //if robot flips(andrew)
+                        SmartDashboard.putNumber("rotationOfRobot.y", rotationCam.getY()); //up and down
+                        SmartDashboard.putNumber("rotationOfRobot.z", rotationCam.getZ()); //left and right
+             
+                }
+        }
+        if(PipelineIndex == 1){//ringdetection
+
+                var imageCaptureTime = res.getTimestampSeconds();
+                var bestTarget = res.getBestTarget();
+                SmartDashboard.putNumber("target.id", bestTarget.getFiducialId());
+                SmartDashboard.putNumber("target.area", bestTarget.getArea());
+
+                double pitch = bestTarget.getPitch(); //degrees
+
+                double camAngToTarget = 90-(angCam-pitch); //degrees
+
+                double camDistToCenterTarget = Math.tan(Math.toRadians(camAngToTarget));
+
+                SmartDashboard.putNumber("DistanceToCenterTarget", camDistToCenterTarget);
+
+                
+
+        }
 }
+}
+     
+        
+        
+
+}
+
+
