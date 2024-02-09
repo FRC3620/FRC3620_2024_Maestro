25a9a2dbff5c11da4494a342714bac990e6490bf
Mik <10108@stjoebears.com>
2024-02-08 20:50:43 -0500

    Made robot drive straighter and made angle motors faster.

--- src/main/java/frc/robot/SuperSwerveController.java
+++ src/main/java/frc/robot/SuperSwerveController.java
@@ -17,9 +17,8 @@ import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
 /** Add your docs here. */
 public class SuperSwerveController {
-    double headingSetpoint;
     PIDController headingPID;
-    private double kSpinP = 0.0055;
+    private double kSpinP = 0.01;
 	private double kSpinI = 0.00000;
 	private double kSpinD = 0.00;
     SwerveSubsystem drivebase;
@@ -28,11 +27,12 @@ public class SuperSwerveController {
     public SuperSwerveController(SwerveSubsystem drivebase) {        
         headingPID = new PIDController(kSpinP, kSpinI, kSpinD);
         headingPID.enableContinuousInput(-180, 180); //sets a circular range instead of a linear one. 
-        headingPID.setTolerance(5);
+        headingPID.setTolerance(3);
         this.drivebase = drivebase;
     } 
 
     Timer turnTimer = null;
+    Double headingSetpoint = null;
 
     public void doTeleop(SwerveSubsystem swerve, double rX, double rY, double rOmega) {
         double targetOmega = 0;
@@ -48,15 +48,17 @@ public class SuperSwerveController {
                 headingSetpoint = drivebase.getHeading().getDegrees();
                 turnTimer.stop();
                 turnTimer.reset();
+            }
+
+            if (headingSetpoint != null) {
                 targetOmega = headingPID.calculate(drivebase.getHeading().getDegrees(), headingSetpoint);
-            } else {
-                targetOmega = 0;
             }
         } else {
             // The driver is providing rotational input. Set Omega according to driver input
             // and store current heading
             turnTimer = null;
             targetOmega = rOmega;
+            headingSetpoint = null;
         }
         
         targetOmega = MathUtil.clamp(targetOmega, -1, 1);
@@ -64,7 +66,9 @@ public class SuperSwerveController {
         swerve.drive(new Translation2d(rX, rY), targetOmega, true);
         SmartDashboard.putNumber("SuperSwerve.targetOmega", targetOmega);
         SmartDashboard.putNumber("SuperSwerve.rOmega", rOmega);
-        SmartDashboard.putNumber("SuperSwerve.headingSetpoint", headingSetpoint);
-
+        if (headingSetpoint != null) {
+            SmartDashboard.putNumber("SuperSwerve.headingSetpoint", headingSetpoint);
+        }
+        SmartDashboard.putNumber("drivebase.getHeading", drivebase.getHeading().getDegrees());
     }
 }
