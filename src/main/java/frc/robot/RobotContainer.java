a6a01c33760e504f467c0fd5f16daf55957b8ca1
Doug Wegscheid <dwegscheid@sbcglobal.net>
2024-02-10 11:42:31 -0500

    Make test build.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,9 +18,9 @@ import org.usfirst.frc3620.logger.LogCommand;
 import org.usfirst.frc3620.logger.EventLogging.Level;
 import org.usfirst.frc3620.misc.CANDeviceFinder;
 
-import frc.robot.commands.SetClimberPositionCommand;
-import frc.robot.subsystems.ClimbElevationSubsystem;
-import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
