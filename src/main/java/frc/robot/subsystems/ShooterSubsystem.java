aa64f26b8b8d32cd763c837650cfbdd9ec64198f
Doug Wegscheid <dwegscheid@sbcglobal.net>
2024-02-11 16:32:35 -0500

    Get the shooter motors right,

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -51,14 +51,14 @@ public class ShooterSubsystem extends SubsystemBase {
 
     configs.MotorOutput.NeutralMode = NeutralModeValue.Coast;
 
-    if (deviceFinder.isDevicePresent(CANDeviceType.TALON_PHOENIX6, 15, "Bottom Shooter") || RobotContainer.shouldMakeAllCANDevices()) {
-      bottomMotor = new TalonFX(15, canBusName);
+    if (deviceFinder.isDevicePresent(CANDeviceType.TALON_PHOENIX6, 14, "Bottom Shooter") || RobotContainer.shouldMakeAllCANDevices()) {
+      bottomMotor = new TalonFX(14, canBusName);
       configMotor("bottom shooter", bottomMotor);
       addChild("bottom", bottomMotor);
     }
 
-    if (deviceFinder.isDevicePresent(CANDeviceType.TALON_PHOENIX6, 14, "Top Shooter") || RobotContainer.shouldMakeAllCANDevices()) {
-      topMotor = new TalonFX(14, canBusName);
+    if (deviceFinder.isDevicePresent(CANDeviceType.TALON_PHOENIX6, 15, "Top Shooter") || RobotContainer.shouldMakeAllCANDevices()) {
+      topMotor = new TalonFX(15, canBusName);
       configMotor("top shooter", topMotor);
       addChild("top", topMotor);
     }
