10a2e5362883bfb1011b50db6250f4a4bd962e41
Doug Wegscheid <dwegscheid@sbcglobal.net>
2024-02-20 18:45:43 -0500

    Tweak power for homing the shooter elevation.

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -189,7 +189,7 @@ public class ShooterSubsystem extends SubsystemBase {
           if (!encoderCalibrated) {
             // If the robot is running, and the encoder is "not calibrated," run motor very
             // slowly towards the switch
-            setElevationPower(0.03);
+            setElevationPower(0.08);
             if (calibrationTimer == null) {
               // we need to calibrate and we have no timer. make one and start it
               calibrationTimer = new Timer();
