b26bda46eda044d3b1bd8af419d0aa2b7a621213
Doug Wegscheid <dwegscheid@sbcglobal.net>
2024-02-20 15:22:25 -0500

    Refactor blinkiness.

---
+++ src/main/java/frc/robot/blinky/LightSegment.java
@@ -0,0 +1,41 @@
+package frc.robot.blinky;
+
+public class LightSegment {
+  int first;
+  int last;
+
+  Pattern currentPattern;
+
+  public LightSegment(int first, int last) {
+    this.first = first;
+    this.last = last;
+  }
+
+  /**
+   * Define what pattern the light segment should display.
+   * 
+   * @param pattern
+   */
+  public void setPattern(Pattern pattern) {
+    if (currentPattern != null) {
+      currentPattern.done(this);
+    }
+    currentPattern = pattern;
+    currentPattern.start(this);
+  }
+
+  public void periodic() {
+    if (currentPattern != null) {
+      currentPattern.periodic(this);
+    }
+  }
+
+  public int getFirst() {
+    return first;
+  }
+
+  public int getLast() {
+    return last;
+  }
+
+}
--- src/main/java/frc/robot/blinky/Pattern.java
+++ src/main/java/frc/robot/blinky/Pattern.java
@@ -1,8 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
-package frc.robot;
+package frc.robot.blinky;
 
 /** Add your docs here. */
 abstract public class Pattern{
