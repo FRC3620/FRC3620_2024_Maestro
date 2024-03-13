280f9a53c8a7460274d2a882aab33179a108a446
Logan <12577@stjoebears.com>
2024-03-12 20:18:55 -0400

    updated Amp Shoot command

--- src/main/java/frc/robot/commands/AmpShootCommand.java
+++ src/main/java/frc/robot/commands/AmpShootCommand.java
@@ -4,22 +4,38 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.IntakeLocation;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSpeedAndAngle;
+import frc.robot.subsystems.ShooterSubsystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class AmpShootCommand extends SequentialCommandGroup {
-  IntakeSubsystem intakeSubsystem = RobotContainer.intakeSubsystem;
+  ShooterSubsystem shooterSubsystem = RobotContainer.shooterSubsystem;
+  ShooterSpeedAndAngle shooterSpeedAndAngle= new ShooterSpeedAndAngle(10, 50);
+  Timer timer= new Timer();
   /** Creates a new AmpShootCommand. */
   public AmpShootCommand() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(
+    /*addCommands(
       new SetIntakeLocationCommand(IntakeLocation.ampPosition)
+    );*/
+    addCommands(
+   new InstantCommand(()->shooterSubsystem.setAmpBarPosition(7)),
+   new WaitUntilCommand(()->shooterSubsystem.getAmpBarPosition()>6),
+  new InstantCommand(()->shooterSubsystem.setSpeedAndAngle(shooterSpeedAndAngle)),
+  new InstantCommand(()-> timer.restart()),
+  new  WaitUntilCommand(()-> timer.get()==0.2),
+  new InstantCommand(()-> shooterSubsystem.setAmpBarPosition(0))
     );
   }
 }
