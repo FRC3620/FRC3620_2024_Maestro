3bd16ae574a84d91292e10fd0d3c4b7eef38a132
Mik <10108@stjoebears.com>
2024-03-25 21:28:51 -0400

    Reverted reversion

--- src/main/java/frc/robot/commands/AutoShooterVisionAngleAdjustmentCommand.java
+++ src/main/java/frc/robot/commands/AutoShooterVisionAngleAdjustmentCommand.java
@@ -54,7 +54,7 @@ public class AutoShooterVisionAngleAdjustmentCommand extends Command {
       SmartDashboard.putNumber("shooterVision.desiredPosition", desiredElevationPosition);
       SmartDashboard.putNumber("shooterVision.actualPosition", actualElevationPosition);
       
-      if (Math.abs(actualElevationPosition - desiredElevationPosition) < 1) {
+      if (Math.abs(actualElevationPosition - desiredElevationPosition) < 2) {
         isFinished = true;
       }
     }
